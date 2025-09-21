51347629ba592f0f42d5edf1188d0fecb630a400
sweetpotato <sweetpotato@users.noreply.github.com>
2025-09-21 22:51:51 +0900

    feat: 小さく表示する画像は小さいサイズでダウンロードする

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -10,6 +10,18 @@ function ImageCard({
 }) {
   const width = small ? 40 : 80
   const height = small ? 56 : 112
+
+  if (imageUrl.startsWith('https://static.wixstatic.com/media/')) {
+    const filename = imageUrl.slice(
+      'https://static.wixstatic.com/media/'.length
+    )
+    imageUrl =
+      'https://static.wixstatic.com/media/' +
+      filename +
+      '/v1/fit/w_80,h_112,q_85,enc_avif,quality_auto/' +
+      filename
+  }
+
   return (
     <>
       <img
